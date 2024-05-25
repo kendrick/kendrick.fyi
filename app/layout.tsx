0ad24a66f545c298f84923e92a33d96a3cab8999
Kendrick Arnett <kmarnett@gmail.com>
2024-05-24 22:55:40 -0500

    Rename #clarity.

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,7 +64,7 @@ export default function RootLayout({
 				src="https://scripts.simpleanalyticscdn.com/latest.js"
 			/>
 			<Script
-				id="clarity"
+				id="ms-clarity"
 				async
 				defer
 				dangerouslySetInnerHTML={{
