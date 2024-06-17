1f551130bb89bf13032f39ec7af4039902dba690
Kendrick Arnett <kmarnett@gmail.com>
2024-06-17 15:36:15 -0500

    Add `style`, `recursive`, and `space` props.

--- lib/layout/Prose/index.tsx
+++ lib/layout/Prose/index.tsx
@@ -1,20 +1,39 @@
+import customPropertiesFromVars from '@/lib/customPropertiesFromVars';
 import clsx from 'clsx';
+import { CSSProperties } from 'react';
 import styles from './styles.module.css';
 
 interface Props {
 	children: React.ReactNode;
 	className?: string | CSSModuleClasses;
+	style?: CSSProperties;
 	maxWidth?: string;
+	space?: string;
+	recursive?: boolean;
 }
 
-export default function Prose({ children, className, maxWidth }: Props) {
-	const style =
-		maxWidth ?
-			({ '--k-prose-max-width': maxWidth } as React.CSSProperties)
-		:	undefined;
+export default function Prose({
+	children,
+	className,
+	style,
+	maxWidth,
+	space,
+	recursive = false,
+}: Props) {
+	const inlineStyle = customPropertiesFromVars('k-prose', style, {
+		maxWidth,
+		space,
+	});
 
 	return (
-		<div style={style} className={clsx([styles.prose, className])}>
+		<div
+			style={inlineStyle}
+			className={clsx([
+				styles.prose,
+				recursive ? styles['prose--recursive'] : '',
+				className,
+			])}
+		>
 			{children}
 		</div>
 	);
