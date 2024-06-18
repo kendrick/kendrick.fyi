4772d1d3515872bae907dd97936d802100df1813
Kendrick Arnett <kmarnett@gmail.com>
2024-06-18 01:50:31 -0500

    Update resume embed.

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { useState } from 'react';
-import { useWindowSize } from 'usehooks-ts';
+import { useEffect, useRef, useState } from 'react';
+import { useDebounceCallback, useResizeObserver } from 'usehooks-ts';
 
 import styles from './styles.module.css';
 
@@ -14,18 +14,61 @@ pdfjs.GlobalWorkerOptions.workerSrc = new URL(
 	import.meta.url,
 ).toString();
 
+function getElementOuterWidth(element: HTMLElement): number {
+	// Ensure the element is an HTMLElement
+	if (!(element instanceof HTMLElement)) {
+		console.error('Provided element is not an HTMLElement:', element);
+		return 0;
+	}
+
+	// Get the bounding client rectangle width
+	const rect = element.getBoundingClientRect();
+	const width = rect.width;
+
+	// Get computed styles
+	const style = window.getComputedStyle(element);
+
+	// Parse and add margin values
+	const marginLeft = parseFloat(style.marginLeft);
+	const marginRight = parseFloat(style.marginRight);
+
+	const totalWidth = width + marginLeft + marginRight;
+
+	return totalWidth;
+}
+
 const Contact = () => {
 	const [numPages, setNumPages] = useState<number>();
 	const [pageNumber, setPageNumber] = useState<number>(1);
-	const [resumeWidth, setResumeWidth] = useState<number>();
-	const { width = 0 } = useWindowSize({ debounceDelay: 200 });
+
+	const ref = useRef<HTMLElement>(null);
+	const [width, setWidth] = useState<number>(0);
+
+	const onResize = useDebounceCallback((entry: ResizeObserverEntry) => {
+		if (entry && ref.current) {
+			const outerWidth = getElementOuterWidth(ref.current);
+			setWidth(outerWidth);
+		}
+	}, 200);
+
+	useResizeObserver({
+		ref,
+		onResize,
+	});
+
+	useEffect(() => {
+		if (ref.current) {
+			const outerWidth = getElementOuterWidth(ref.current);
+			setWidth(outerWidth);
+		}
+	}, []);
 
 	function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
 		setNumPages(numPages);
 	}
 
 	return (
-		<section style={{ margin: 'auto' }}>
+		<section ref={ref} style={{ margin: 'var(--k-space-xs-xl)' }}>
 			<Document
 				file="/kendrick-arnett-resume.pdf"
 				onLoadSuccess={onDocumentLoadSuccess}
