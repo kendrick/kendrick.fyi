e465be189d601b8381b1081fa79ad91aaac5560d
Kendrick Arnett <kmarnett@gmail.com>
2024-06-21 12:47:25 -0500

    Fix broken ref.

--- components/skills/index.tsx
+++ components/skills/index.tsx
@@ -1,16 +1,13 @@
 'use client';
 
 import { skillsData } from '@/lib/data';
-import { useSectionInView } from '@/lib/hooks';
 
 import Cluster from '../../lib/layout/Cluster';
 import styles from './styles.module.css';
 
 export default function Skills() {
-	const { ref } = useSectionInView('Skills');
-
 	return (
-		<section id="skills" ref={ref} className={styles.skills}>
+		<section id="skills" className={styles.skills}>
 			<h2>Skills</h2>
 			<Cluster className={styles.skills__list}>{skillsData}</Cluster>
 		</section>
